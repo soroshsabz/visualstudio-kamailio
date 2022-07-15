37adda9eaa8576a62316034455adf3b0c432ae32
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2022-07-15 20:36:52 +0430

    Add TextMate grammar files.

---
+++ Source/Kamailio.VisualStudio/KamailioContentTypeDefinition.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamailio.VisualStudio
+{
+#pragma warning disable 649
+    public class KamailioContentDefinition
+    {
+        [Export]
+        [Name("kamailio")]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        internal static ContentTypeDefinition KamailioContentTypeDefinition;
+
+        [Export]
+        [FileExtension(".cfg")]
+        [ContentType("kamailio")]
+        internal static FileExtensionToContentTypeDefinition KamailioFileExtensionDefinition;
+    }
+#pragma warning restore 649
+}
